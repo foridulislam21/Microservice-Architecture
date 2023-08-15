7fa32925167218a3f81f09266dcde4dacaf499d0
foridulislam21 <foridulislam21@gmail.com>
2023-08-16 02:30:01 +0600

    adding search functionality from mongo db

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
+        {
+            var query = DB.PagedSearch<Item>();
+            query.Sort(x => x.Ascending(a => a.Make));
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                query.Match(Search.Full, searchTerm).SortByTextScore();
+            }
+            query.PageNumber(pageNumber);
+            query.PageSize(pageSize);
+            var result = await query.ExecuteAsync();
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
+        }
+    }
+}
