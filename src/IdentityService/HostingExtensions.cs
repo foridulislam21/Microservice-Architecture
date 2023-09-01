1ebfc5b0d7b4d57469157eaa3120e6f56c0a9131
foridulislam21 <foridulislam21@gmail.com>
2023-09-02 00:35:10 +0600

    working on identity server

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -34,7 +34,10 @@ internal static class HostingExtensions
             .AddInMemoryApiScopes(Config.ApiScopes)
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
-        
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.SameSite = SameSiteMode.Lax;
+        });
         builder.Services.AddAuthentication();
 
         return builder.Build();
