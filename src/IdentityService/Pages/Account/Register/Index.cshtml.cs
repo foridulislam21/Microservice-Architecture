f38d0694700d9cfefcede1eee504167a6c1c6c16
foridulislam21 <foridulislam21@gmail.com>
2023-09-21 01:14:45 +0600

    working on registration

--- src/IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -1,22 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace IdentityService.Pages.Account.Register
 {
+    [SecurityHeaders]
+    [AllowAnonymous]
     public class Index : PageModel
     {
-        private readonly ILogger<Index> _logger;
-
-        public Index(ILogger<Index> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
         }
