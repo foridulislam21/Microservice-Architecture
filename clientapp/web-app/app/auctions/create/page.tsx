b77c151683c0012faef2ec3793ccfd98b95363b9
foridulislam21 <foridulislam21@gmail.com>
2023-12-04 01:16:29 +0600

    changes

--- clientapp/web-app/app/auctions/create/page.tsx
+++ clientapp/web-app/app/auctions/create/page.tsx
@@ -1,5 +1,13 @@
+import Heading from "@/app/components/Heading";
 import React from "react";
 
 export default function Cteate() {
-  return <div>Cteate</div>;
+  return (
+    <div className="mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+      <Heading
+        title="Sell your car!"
+        subtitle="Please enter the details of your car"
+      ></Heading>
+    </div>
+  );
 }
--- clientapp/web-app/types/next-auth.d.ts
+++ clientapp/web-app/types/next-auth.d.ts
@@ -2,6 +2,7 @@ import { DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      id: string;
       username: string;
     } & DeFaultSession["user"];
   }
